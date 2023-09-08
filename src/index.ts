4434c4bb5d29c3ef3983aa832927523af7553b7d
WMXPY <wm@mengw.io>
2023-09-07 23:45:44 -0700

    release 1.1.0

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
  */
 
 export * from "./v1/account/post-register";
+export * from "./v1/authentication/post-acquire";
 export * from "./v1/authentication/post-inquiry";
 export * from "./v1/authentication/post-realize";
 export * from "./v1/authentication/post-redeem";
